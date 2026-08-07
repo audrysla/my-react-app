8a72906ab0b6ab8678b58f29c280852a954a24af
audry <kmkyou1@nate.com>
2026-08-08 05:45:42 +0900

    깃 호스팅 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.scss'
 import { Header } from './components/layout/Header'
 import { Contents } from './components/layout/Contents'
