65c027fd635ac594e66d968ee2f59628724c194c
audry <kmkyou1@nate.com>
2026-08-09 03:19:36 +0900

    수정

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,15 +3,15 @@ import { UserProfile } from '../components/member/UserProfile'
 export default function About() {
   return (
     <>
-      <h3>유저 프로필</h3>
-        <p>(interface 프롭스)</p>
-        <UserProfile 
-          name="김명교"
-          age={43}
-          job="웹퍼블리셔"
-          skills="react, vue, javascript, typescript, html, css, scss, git, api"
-          email="myoung_kyo@hanmail.net"
-          notes="jsp, asp, php"/>
+      <h2>유저 프로필</h2><br/>
+      <h3>interface 프롭스</h3>
+      <UserProfile 
+        name="김명교"
+        age={43}
+        job="웹퍼블리셔"
+        skills="react, vue, javascript, typescript, html, css, scss, git, api"
+        email="myoung_kyo@hanmail.net"
+        notes="jsp, asp, php"/>
     </>
   );
 }
