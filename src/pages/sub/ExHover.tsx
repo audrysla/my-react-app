6b882920b7603898606fa6da169605ae46e44ed2
audry <kmkyou1@nate.com>
2026-08-10 07:50:50 +0900

    수정

--- src/pages/sub/ExHover.tsx
+++ src/pages/sub/ExHover.tsx
@@ -1,10 +1,21 @@
-import temp1 from '../../assets/images/temp.png'
+import { useState } from 'react';
 
 export default function ExHover() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <>
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        display: 'inline-block',
+        width: '280px',
+      }}
+    >
       <h2>hover 페이지</h2>
-      <img src={temp1} alt='공사중' />
-    </>
+
+      {isHovered && <span>✨✨✨✨✨✨✨</span>}
+      
+    </div>
   );
 }
