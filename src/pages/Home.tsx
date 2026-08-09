fa78c719ff0aa561f1063a319dfa4fd3c97ca230
audry <kmkyou1@nate.com>
2026-08-10 06:44:20 +0900

    수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <h3>Typescript 적용 컴포넌트</h3>      
         <br/>
         <p>{status}</p>
-        <h5>카운트 : {count}</h5>
+        <h5>카운트 : <em key={count} className='countNum'>{count}</em></h5>
         <br/>
         <div>        
           <Button onClick={handleClick}>클릭!</Button>
